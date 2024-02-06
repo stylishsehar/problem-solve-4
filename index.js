ee438400667812a2db4c5da6c6a08c9b0d429949
stylishsehar <stylishsehar262@gmail.com>
2024-02-06 17:18:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// no:1
+var name = prompt("Enter first name")
+alert(name)
+
+// no:2
+ var n = prompt("country")
+ alert(n)
+
+//no:3
+var yourName = prompt("Enter Your Name");
+alert(yourName)
+
+// no:5
+var s = prompt("please enter your password" , "23456")
+alert(s)
+// no:6
+let userresponse = prompt("please type in your response")
+alert(userresponse)
